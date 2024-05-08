fc167e35ef023b1effbe234dd540797641a4ff5d
Markfady <markfady9@gmail.com>
2024-05-08 11:27:09 +0300

    call product List inside Module & app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'pm-root',
   template: `<div>
     <h1>{{ pageTitle }}</h1>
+    <pm-products> </pm-products>
   </div>`,
 })
 export class AppComponent {
