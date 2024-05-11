8c1e29249fbf4435b5a0642d5d5535a85f567fea
Markfady <markfady9@gmail.com>
2024-05-12 00:01:59 +0300

    refactor app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { FormsModule } from '@angular/forms';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
     WelcomeComponent,
-    StarComponent,
-    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule, //To use ngModel custom directive inside product-list.component.html
     HttpClientModule, //To use Http inside service file , to make get request that returns observable,
     RouterModule.forRoot([
-      {path:'products',component:ProductListComponent},
-      {path:'products/:id', canActivate:[ProductDetailGuard],component:ProductDetailComponent},
       {path:'welcome',component:WelcomeComponent},
       {path:'',redirectTo:'welcome',pathMatch:'full'}, //when application loads this will be the default view of our application
       {path:'**',redirectTo:'welcome',pathMatch:'full'}
-    ]) //To setup routing 
+    ]), ProductModule 
   ],
   bootstrap: [AppComponent]
 })
