e967f846fe14273b38b76358196ec9437b9c8266
Markfady <markfady9@gmail.com>
2024-05-11 12:39:01 +0300

    handle get , http service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,33 +1,36 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from "./products";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable,catchError,tap, throwError } from "rxjs";
 
 @Injectable({
     providedIn:'root'
 })
  //a service to only get the products data so we can call it when we want , better than putting the hardcoded data in each component
 export class ProductService{
-    getProducts():IProduct[]{
-        return [
-            {
-                "productId": 1,
-                "productName": "Leaf Rake",
-                "productCode": "GDN-0011",
-                "releaseDate": "March 19, 2021",
-                "description": "Leaf rake with 48-inch wooden handle.",
-                "price": 19.95,
-                "starRating": 3.2,
-                "imageUrl": "assets/images/leaf_rake.png"
-              },
-              {
-                "productId": 2,
-                "productName": "Garden Cart",
-                "productCode": "GDN-0023",
-                "releaseDate": "March 18, 2021",
-                "description": "15 gallon capacity rolling garden cart",
-                "price": 32.99,
-                "starRating": 4.2,
-                "imageUrl": "assets/images/garden_cart.png"
-              },
-        ]
+    private productUrl= 'api/products/products.json'
+    constructor(private http:HttpClient){
+
+    }
+    getProducts():Observable<IProduct[]>{
+        return this.http.get<IProduct[]>(this.productUrl).pipe(
+            tap(data=>console.log('all',JSON.stringify(data))), //convert array of object to JSON string
+            catchError(this.handleError)
+        );
     }   
+    private handleError(err: HttpErrorResponse): Observable<never> {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent) {
+          // A client-side or network error occurred. Handle it accordingly.
+          errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(() => errorMessage);
+      }
 }
