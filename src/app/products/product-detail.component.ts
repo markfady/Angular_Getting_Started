818128fac06d7a14a34e47f0787d93e919c1bf6e
Markfady <markfady9@gmail.com>
2024-05-11 15:52:29 +0300

    get parameter from url using activatedRoute

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   //selector: 'pm-product-detail', //we only need selector if we will nest this component 
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle:string='Product Details'
-  constructor() { }
+  constructor(private route:ActivatedRoute) { } //ActivatedRoute to read the URL and get the parameter , to display clicked product Info
 
-  ngOnInit(): void {
+  ngOnInit(): void { //initiated when component initialized (Like useEffect)
+    const id=Number(this.route.snapshot.paramMap.get('id')); //parameter(will not change so we use snapshot)
+    this.pageTitle +=`product id = ${id} `
   }
+
 
 }
