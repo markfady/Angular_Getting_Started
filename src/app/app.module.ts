3823e847c5e89ac21f27954fab498d38e41e656f
Markfady <markfady9@gmail.com>
2024-05-11 14:28:28 +0300

    router paths setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,30 @@ import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    WelcomeComponent,
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule, //To use ngModel custom directive inside product-list.component.html
-    HttpClientModule, //To use Http inside service file , to make get request that returns observable
+    HttpClientModule, //To use Http inside service file , to make get request that returns observable,
+    RouterModule.forRoot([
+      {path:'products',component:ProductListComponent},
+      {path:'products/:id',component:ProductDetailComponent},
+      {path:'welcome',component:WelcomeComponent},
+      {path:'',redirectTo:'welcome',pathMatch:'full'}, //when application loads this will be the default view of our application
+      {path:'**',redirectTo:'welcome',pathMatch:'full'}
+    ]) //To setup routing 
   ],
   bootstrap: [AppComponent]
 })
