8d4001adbd64d5c140cc652e6400eabab255d8c8
Markfady <markfady9@gmail.com>
2024-05-12 12:21:38 +0300

    setup route for customer Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,22 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
 import { ProductModule } from './products/product.module';
+import { CustomerComponent } from './reactiveForm/customer/customer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
+    CustomerComponent,
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     HttpClientModule, //To use Http inside service file , to make get request that returns observable,
     RouterModule.forRoot([
       {path:'welcome',component:WelcomeComponent},
+      {path:'ReactiveForm',component:CustomerComponent},
       {path:'',redirectTo:'welcome',pathMatch:'full'}, //when application loads this will be the default view of our application
       {path:'**',redirectTo:'welcome',pathMatch:'full'}
     ]), ProductModule 
