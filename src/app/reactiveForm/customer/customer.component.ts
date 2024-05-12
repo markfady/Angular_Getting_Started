045e596e4920f2b7b608928130a552ac206fa98a
Markfady <markfady9@gmail.com>
2024-05-12 14:35:20 +0300

    Form builder reduce boilerplate code

--- src/app/reactiveForm/customer/customer.component.ts
+++ src/app/reactiveForm/customer/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
@@ -11,16 +11,24 @@ export class CustomerComponent implements OnInit {
   customerForm: FormGroup //defines our form Model (needs to be initialized 'in decleration or constructor or in ngOnInit')
   customer= new Customer(); //defines data passed to and from back-end server
 
-  constructor() {
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true)
-    });
+  constructor(private formBuilder:FormBuilder) {
+    this.customerForm=this.formBuilder.group({
+      firstName:'',
+      lastName: '',
+      email: '',
+      sendCatalog: true
+    })
   }
   
   ngOnInit(){
+  }
+  populateTestData():void{
+    this.customerForm.setValue({
+      firstName:'Mark',
+      lastName:'Fady',
+      email:'mark@giza.com',
+      sendCatalog:false
+    })
   }
   save(){
     console.log(this.customerForm)
