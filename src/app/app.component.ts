6f6925c645b07ce6f419dc17b2acf59aae410b25
Markfady <markfady9@gmail.com>
2024-05-13 16:22:20 +0300

    link to Add new products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
         <ul class='nav nav-pills'>
           <li><a class='nav-link' routerLink='/welcome'>Home</a></li>
           <li><a class='nav-link' routerLink='/products'>Product List</a></li>
+          <li><a class='nav-link' [routerLink]="['/products/0/edit']">Add Product</a></li>
           <li><a class='nav-link' routerLink='/ReactiveForm'>Form</a></li>
         </ul>
     </nav>
