f1d118ad09d1aecf95ec4d6a0d13de33c88350f9
Markfady <markfady9@gmail.com>
2024-05-13 15:40:55 +0300

    pass the selected product id to getProduct service

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './products';
+import { ProductService } from './product.service';
 
 @Component({
   //selector: 'pm-product-detail', //we only need selector if we will nest this component 
@@ -7,13 +9,41 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle:string='Product Details'
-  constructor(private route:ActivatedRoute, private router:Router) { } //ActivatedRoute to read the URL and get the parameter , to display clicked product Info
+  errorMessage=''
+  product: IProduct | undefined;
+  constructor(private route:ActivatedRoute, private router:Router,private productService:ProductService) { 
+
+
+  } //ActivatedRoute to read the URL and get the parameter , to display clicked product Info
 
   ngOnInit(): void { //initiated when component initialized (Like useEffect)
     const id=Number(this.route.snapshot.paramMap.get('id')); //parameter(will not change so we use snapshot)
-    this.pageTitle +=`product id = ${id} `
+
+
+      if(id){
+        this.getProduct(id);
+        console.log(id)
+      }
+
   }
+  
+  getProduct(id: number): void {
+    this.productService.getProducts().subscribe(
+      (products) => {
+        const product = products.find(p => p.productId === id);
+        if (product) {
+          this.product = product;
+        } else {
+          this.errorMessage = 'Product not found';
+        }
+      },
+      (error) => {
+        this.errorMessage = 'Error fetching products';
+        console.error(error);
+      }
+    );
+  }
+
   onBack():void{
     this.router.navigate(['/products']);
   }
