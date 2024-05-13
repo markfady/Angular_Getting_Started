b1ed37487b09486f5e268a0a8c608f2f9cc7040f
Markfady <markfady9@gmail.com>
2024-05-13 15:41:19 +0300

    getProduct method to get info of product

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from "./products";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Observable,catchError,tap, throwError } from "rxjs";
+import { Observable,catchError,of,tap, throwError } from "rxjs";
 
 @Injectable({
     providedIn:'root'
@@ -9,15 +9,26 @@ import { Observable,catchError,tap, throwError } from "rxjs";
  //a service to only get the products data so we can call it when we want , better than putting the hardcoded data in each component
 export class ProductService{
     private productUrl= 'api/products/products.json'
-    constructor(private http:HttpClient){
+    constructor(private http:HttpClient){}
 
-    }
     getProducts():Observable<IProduct[]>{
         return this.http.get<IProduct[]>(this.productUrl).pipe(
             tap(data=>console.log('all',JSON.stringify(data))), //convert array of object to JSON string
             catchError(this.handleError)
         );
     }   
+    
+    getProduct(id: number): Observable<IProduct> {
+      if (id === 0) {
+        return of(this.initializeProduct());
+      }
+      const url = `${this.productUrl}/${id}`;
+      return this.http.get<IProduct>(url)
+        .pipe(
+          tap(data => console.log('getProduct: ' + JSON.stringify(data))),
+          catchError(this.handleError)
+        );
+    }
     private handleError(err: HttpErrorResponse): Observable<never> {
         // in a real world app, we may send the server to some remote logging infrastructure
         // instead of just logging it to the console
@@ -33,4 +44,17 @@ export class ProductService{
         console.error(errorMessage);
         return throwError(() => errorMessage);
       }
+      private initializeProduct(): IProduct {
+        // Return an initialized object
+        return {
+          productId:0,
+          productName: '',
+          productCode: '',
+          releaseDate: '',
+          price: 0,
+          description: '',
+          starRating: 0,
+          imageUrl: '',
+        };
+}
 }
