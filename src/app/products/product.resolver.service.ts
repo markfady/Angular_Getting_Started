2d806942563a0c1545853c6587c1fe5c8984e080
Markfady <markfady9@gmail.com>
2024-05-16 16:31:10 +0300

    product resolver service 'prefetch data'

--- src/app/products/product.resolver.service.ts
+++ src/app/products/product.resolver.service.ts
@@ -10,9 +10,9 @@ import { Observable } from "rxjs";
 
 export class ProductResolver implements Resolve<IProduct>{
     constructor(private productService: ProductService) { }
-//comment
+
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IProduct> {
-       //test
+       
             const id = Number(route.paramMap.get('id')) ;
             return this.productService.getProduct(id);
         }
