55dc837cd09669b2ef18ce5be8a2b6d1c4528a28
Mark Fady <markfady9@email.com>
2024-05-16 16:07:55 +0300

    c

--- src/app/products/product.resolver.service.ts
+++ src/app/products/product.resolver.service.ts
@@ -12,7 +12,7 @@ export class ProductResolver implements Resolve<IProduct>{
     constructor(private productService: ProductService) { }
 //commenttt
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IProduct> {
-       //testt
+       //test
             const id = Number(route.paramMap.get('id')) ;
             return this.productService.getProduct(id);
         }
