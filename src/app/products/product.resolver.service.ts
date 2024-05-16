4c7b7fb5cec0890705cbb747481ff0de27f471c4
Markfady <markfady9@gmail.com>
2024-05-16 17:06:12 +0300

    public resolver using id to prefetch data

--- src/app/products/product.resolver.service.ts
+++ src/app/products/product.resolver.service.ts
@@ -1,20 +1,40 @@
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
-import { IProduct } from "./products";
-import { ProductService } from "./product.service";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+
+import { ProductService } from './product.service';
+import { IProduct, ProductResolved } from './products';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
+export class ProductResolver implements Resolve<ProductResolved> {
 
-export class ProductResolver implements Resolve<IProduct>{
-    constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) { }
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IProduct> {
-       
-            const id = Number(route.paramMap.get('id')) ;
-            return this.productService.getProduct(id);
-        }
+  resolve(route: ActivatedRouteSnapshot,
+          state: RouterStateSnapshot): Observable<ProductResolved> {
+    const id = Number(route.paramMap.get('id'));
+    if (isNaN(id)) {
+      const message = `Product id was not a number: ${id}`;
+      console.error(message);
+      return of({ product: null as unknown as IProduct, error: message });
+
+
     }
 
+    return this.productService.getProduct(+id)
+      .pipe(
+        map(product => ({ product })),
+        catchError(error => {
+          const message = `Retrieval error: ${error}`;
+          console.error(message);
+          return of({ product: null as unknown as IProduct, error: message });
+        })
+      );
+  }
+
+}
